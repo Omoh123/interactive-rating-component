9f679d5c1e4d42f819f66d059bca44233645ddb0
Omoh123 <107009489+Omoh123@users.noreply.github.com>
2022-12-30 03:36:19 +0100

    update
    made some necessary changes

---
+++ script.js
@@ -0,0 +1,23 @@
+/*const mainContainer = document.querySelector(".main-container");
+ const thankYou = document.querySelector(".thankyou-container");
+ const btn = document.getElementById("btn");
+ const rates = document.querySelectorAll(".no");
+ const rating = document.querySelector(".rating");
+ const rateAgain = document.getElementById("rate-again")
+
+
+ btn.addEventListener("click", () =>{
+    thankYou.classList.remove("hidden");
+    mainContainer.style.display = "none";
+ })
+
+ rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+ rating.innerHTML = rate.innerHTML;
+    })
+ });
+
+rateAgain.addEventListener("click", () =>{
+    mainContainer.style.display ="block";
+    thankYou.classList.add("hidden");
+})*/
